e4343c5d1060bf3dabb555564e041ba23b16de76
Simon Alphonse <IAmSimon@outlook.in>
2022-12-05 16:02:38 +0530

    Refactored Part 2

--- 2022/5.SupplyStacks/Program.cs
+++ 2022/5.SupplyStacks/Program.cs
@@ -9,7 +9,7 @@ namespace SupplyStacks
     {
         record Instruction(int Count, int From, int To);
 
-        static void Main(string[] args)
+        static void Main()
         {
             var inputs = File.ReadAllText($"inputs.txt")
                 .Split($"{Environment.NewLine}{Environment.NewLine}")
@@ -18,29 +18,39 @@ namespace SupplyStacks
             var supplies = inputs.First().TakeWhile(x => x.Contains("[")).Reverse()
                 .Select(s => s.Chunk(4).Select(s => string.Concat(s).Trim()).ToList()).ToList();
 
-            var stacks = new Dictionary<int, List<string>>();
-
-            for (int stackIndex = 1; stackIndex <= supplies.Select(s => s.Count).Max(); stackIndex++)
-                stacks.Add(stackIndex, supplies.Select(s => s.Skip(stackIndex - 1).First()).Where(w => !string.IsNullOrWhiteSpace(w)).ToList());
-
             var instructions = inputs.Last().Select(s =>
                 s.Split(" ", StringSplitOptions.RemoveEmptyEntries)
                  .Where(w => int.TryParse(w, out int temp))
                  .Select(int.Parse))
-                 .Select(x => new Instruction (Count: x.First(), From: x.Skip(1).First(), To: x.Last())).ToList();
+                 .Select(x => new Instruction(Count: x.First(), From: x.Skip(1).First(), To: x.Last())).ToList();
+
+            RearrangeSupplies(inputs, supplies, instructions, true);
+            RearrangeSupplies(inputs, supplies, instructions, false);
+
+            Console.Read();
+        }
+
+        private static void RearrangeSupplies(List<string[]> inputs, List<List<string>> supplies, List<Instruction> instructions, bool isSinglePickup)
+        {
+            var stacks = new Dictionary<int, List<string>>();
+
+            for (int stackIndex = 1; stackIndex <= supplies.Select(s => s.Count).Max(); stackIndex++)
+                stacks.Add(stackIndex, supplies.Select(s => s.Skip(stackIndex - 1).First()).Where(w => !string.IsNullOrWhiteSpace(w)).ToList());
 
             foreach (var instruction in instructions)
             {
                 var sourceStack = stacks[instruction.From];
                 var suppliesToMove = sourceStack.TakeLast(instruction.Count).ToArray();
 
-                stacks[instruction.To].AddRange(suppliesToMove.Reverse());
+                if (isSinglePickup)
+                    stacks[instruction.To].AddRange(suppliesToMove.Reverse());
+                else
+                    stacks[instruction.To].AddRange(suppliesToMove);
+                
                 stacks[instruction.From].RemoveRange(stacks[instruction.From].Count - instruction.Count, instruction.Count);
             }
 
-            Console.WriteLine($"Part One : {string.Concat(stacks.Select(s => s.Value.Last())).Replace("[", string.Empty).Replace("]", string.Empty)}");
-
-            Console.Read();
+            Console.WriteLine($"Part {(isSinglePickup ? "One" : "Two")}: {string.Concat(stacks.Select(s => s.Value.Last())).Replace("[", string.Empty).Replace("]", string.Empty)}");
         }
     }
 }
