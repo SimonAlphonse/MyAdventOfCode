9971c60b4f2ad246fd11cab342fe9ead27e19029
Simon Alphonse <IAmSimon@outlook.in>
2022-12-05 09:13:59 +0530

    Shortened the logic

--- 2022/1.CalorieCounting/Program.cs
+++ 2022/1.CalorieCounting/Program.cs
@@ -1,25 +1,11 @@
-﻿var lines = File.ReadAllLines("inputs.txt");
-
-List<List<int>> elves = new() { new () };
-
-foreach (var line in lines)
-{
-    List<int> calories = elves.Last();
-
-    if (int.TryParse(line, out var calorie))
-        calories.Add(calorie);
-    else
-        elves.Add(new ());
-}
+﻿var lines = File.ReadAllText("inputs.txt");
+var elves = lines.Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(s=> s.Split($"{Environment.NewLine}").Select(int.Parse).ToList()).ToList();
 
 var elvesByCalories = elves.OrderByDescending(o => o.Sum()).ToList();
 
-var maxCalories = elvesByCalories.First().Sum();
+Console.WriteLine($"Part One : {elvesByCalories.First().Sum()}");
 
-Console.WriteLine($"First : {maxCalories}");
-
-var top3Calories = elvesByCalories.Take(3).SelectMany(s => s).Sum();
-
-Console.WriteLine($"Second : {top3Calories}");
+Console.WriteLine($"Part Two : {elvesByCalories.Take(3).SelectMany(s => s).Sum()}");
 
 Console.Read();
