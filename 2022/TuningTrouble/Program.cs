f00c3db5432193e288a89f52ff8c732a4636f102
Simon Alphonse <IAmSimon@outlook.in>
2022-12-06 10:54:30 +0530

    Day 6 - Tuning Trouble

---
+++ 2022/TuningTrouble/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TuningTrouble
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Part One :{FindMarkerIndex(4)}"); ;
+            Console.WriteLine($"Part Two :{FindMarkerIndex(14)}"); ;
+
+            Console.Read();
+        }
+
+        private static int FindMarkerIndex(int markerLength)
+        {
+            var lines = File.ReadAllText($"inputs.txt");
+
+            for (int i = 0; i < lines.Length - 3; i++)
+            {
+                string marker = string.Concat(lines.Skip(i).Take(markerLength));
+                if (marker.Distinct().Count() == markerLength)
+                {
+                    return i + markerLength;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
