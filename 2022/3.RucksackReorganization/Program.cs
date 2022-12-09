bbaf8671c34c562b1dd98ba261c5aac7e0822ef5
Simon Alphonse <IAmSimon@outlook.in>
2022-12-09 23:19:24 +0530

    Code change inspired by github.com/encse

--- 2022/3.RucksackReorganization/Program.cs
+++ 2022/3.RucksackReorganization/Program.cs
@@ -6,27 +6,19 @@ public abstract class Program
     {
         var lines = File.ReadAllLines("inputs.txt");
 
-        var priorities =
-            Enumerable.Range('a', 'z' - 'a' + 1).Select((s, i) => KeyValuePair.Create((char)s, i + 1)).Concat(
-            Enumerable.Range('A', 'Z' - 'A' + 1).Select((s, i) => KeyValuePair.Create((char)s, i + 1 + 26)))
-                .ToDictionary(x => x.Key, y => y.Value);
+        var priorities = Enumerable.Range('a', 'z' - 'a' + 1).Select((s, i) => KeyValuePair.Create((char)s, i + 1))
+            .Concat(Enumerable.Range('A', 'Z' - 'A' + 1).Select((s, i) => KeyValuePair.Create((char)s, i + 1 + 26)))
+            .ToDictionary(x => x.Key, y => y.Value);
 
-        var rucksacks = (from line in lines
-                let mid = line.Length / 2
-                select (First: line.Take(mid), Second: line.TakeLast(mid)))
-            .Select(s => (s.First, s.Second, Common: s.First.Intersect(s.Second).First()))
-            .Select(s => (s.First, s.Second, s.Common, Priority: priorities[s.Common])).ToList();
+        var markers = lines.Select(line => line.Chunk(line.Length / 2)).Select(GetCommonItem);
+        Console.WriteLine($"Part One : {markers.Sum(s => priorities[s])}");
 
-        Console.WriteLine($"Part One : {rucksacks.Sum(s => s.Priority)}");
-
-        var groups = lines.Select(s => string.Concat(s.Distinct())).Chunk(3).ToList();
-        var flattenGroups = groups.Select(s => string.Concat(s)).ToList();
-        var badges = flattenGroups.Select(s => s.GroupBy(g => g)
-                .OrderByDescending(o => o.Count()).First().Key)
-                .Select(s => (Badge: s, Priority: priorities[s])).ToList();
-
-        Console.WriteLine($"Part Two : {badges.Sum(s => s.Priority)}");
+        var badges = lines.Chunk(3).Select(GetCommonItem);
+        Console.WriteLine($"Part Two : {badges.Sum(s => priorities[s])}");
 
         Console.ReadKey();
     }
+
+    private static T GetCommonItem<T>(IEnumerable<IEnumerable<T>> chunk) =>
+        chunk.First().First(t => chunk.All(a => a.Contains(t)));
 }
