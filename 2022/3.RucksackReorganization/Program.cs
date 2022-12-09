59a00114cff3d0ad3534563f03e898677bfb7f6d
Simon Alphonse <IAmSimon@outlook.in>
2022-12-09 22:17:48 +0530

    Cleanup

--- 2022/3.RucksackReorganization/Program.cs
+++ 2022/3.RucksackReorganization/Program.cs
@@ -1,26 +1,31 @@
-﻿public class Program
+﻿namespace RucksackReorganization;
+
+public abstract class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         var lines = File.ReadAllLines("inputs.txt");
+
         var priorities =
-            Enumerable.Range(97, 26).Select((s, i) => KeyValuePair.Create((char)s, i + 1)).Concat(
-            Enumerable.Range(65, 26).Select((s, i) => KeyValuePair.Create((char)s, i + 27))).ToDictionary(x => x.Key, y => y.Value);
-        
-        var rucksacks = (from line in lines 
-                let mid = line.Length
+            Enumerable.Range('a', 'z' - 'a' + 1).Select((s, i) => KeyValuePair.Create((char)s, i + 1)).Concat(
+            Enumerable.Range('A', 'Z' - 'A' + 1).Select((s, i) => KeyValuePair.Create((char)s, i + 1 + 26)))
+                .ToDictionary(x => x.Key, y => y.Value);
+
+        var rucksacks = (from line in lines
+                let mid = line.Length / 2
                 select (First: line.Take(mid), Second: line.TakeLast(mid)))
-            .Select(s => (s.First, s.Second, Common: Enumerable.Intersect(s.First, s.Second).First()))
+            .Select(s => (s.First, s.Second, Common: s.First.Intersect(s.Second).First()))
             .Select(s => (s.First, s.Second, s.Common, Priority: priorities[s.Common])).ToList();
 
-        Console.WriteLine($"First : {rucksacks.Sum(s => s.Priority)}");
+        Console.WriteLine($"Part One : {rucksacks.Sum(s => s.Priority)}");
 
         var groups = lines.Select(s => string.Concat(s.Distinct())).Chunk(3).ToList();
         var flattenGroups = groups.Select(s => string.Concat(s)).ToList();
-        var badges = flattenGroups.Select(s => s.GroupBy(g => g).OrderByDescending(o => o.Count()).First().Key)
-            .Select(s => (Badge: s, Priority: priorities[s])).ToList();
+        var badges = flattenGroups.Select(s => s.GroupBy(g => g)
+                .OrderByDescending(o => o.Count()).First().Key)
+                .Select(s => (Badge: s, Priority: priorities[s])).ToList();
 
-        Console.WriteLine($"Second : {badges.Sum(s => s.Priority)}");
+        Console.WriteLine($"Part Two : {badges.Sum(s => s.Priority)}");
 
         Console.ReadKey();
     }
