346290f594076fd93637f488b797d590b7add577
Simon Alphonse <IAmSimon@outlook.in>
2022-12-11 10:26:15 +0530

    Need to fix last column

--- 2022/10.CathodeRayTube/Program.cs
+++ 2022/10.CathodeRayTube/Program.cs
@@ -5,39 +5,46 @@ namespace CathodeRayTube
     internal abstract class Program
     {
         record Instruction(string Command, int Cycle, int Value);
-        record Cycle(int No, int Register, int[] Spire);
+        record Cycle(int No, int[] Spire);
+        record State(Instruction Instruction, int From, int To);
 
         public static void Main()
         {
-            var instructions = File.ReadAllLines($"inputs-demo.txt")
+            var instructions = File.ReadAllLines($"inputs.txt")
                 .Select(s => s.Replace("noop", "noop 1 0")
                     .Replace("addx ", "addx 2 ")).Select(s => s.Split(" "))
                 .Select(s => new Instruction(s[0], int.Parse(s[1]), int.Parse(s[2]))).ToList();
 
-            List<int> registers = new() { 1 };
-            foreach (var instruction in instructions)
-                for (var i = 1; i <= instruction.Cycle; i++)
-                    registers.Add(registers.Last() + (i == instruction.Cycle ? instruction.Value : 0));
-
-            var bookmarks = new[] { 20, 60, 100, 140, 180, 220 };
-            var result = registers.Where((value, index) => bookmarks.Contains(index + 1))
-                .Select((value, index) => value * bookmarks[index]).Sum();
+            List<State> registers = ExecuteInstructions(instructions);
+            
+            var bookmarks = Enumerable.Range(-20, 6).Select(s => s + 40).ToArray();
+            var result = registers.Where((_, index) => bookmarks.Contains(index + 1))
+                .Select((value, index) => value.From * bookmarks[index]).Sum();
 
             Console.WriteLine($"Part One : {result}");
 
             char[] crt = new string(' ', 40 * 6).ToCharArray();
 
-            var cycles = registers.Select((s, i) => new Cycle(i, s, new[] { s - 1, s, s + 1 })).ToArray();
+            var cycles = registers.Skip(1).Select((s, i) =>
+                new Cycle(i + 1, new[] { s.From % 40 - 1, s.From % 40, s.From % 40 + 1 })).ToArray();
 
-            foreach (var cycle in cycles.Skip(1))
-            {
-                if (cycle.Spire.Contains(cycle.No - 1) || cycles[cycle.No - 1].Spire.Contains(cycle.No - 1))
-                    crt[cycle.No - 1] = '#';
-                else
-                    crt[cycle.No - 1] = '.';
-            }
+            foreach (var cycle in cycles)
+                crt[cycle.No - 1] = cycle.Spire.Contains(Math.Max(cycle.No % 40 - 1, 0)) ? '#' : '.';
 
             PrettyPrint(crt);
+        }
+
+        private static List<State> ExecuteInstructions(List<Instruction> instructions)
+        {
+            List<State> registers = new() { new State(new("Start", 0, 0), 1, 1) };
+
+            foreach (var instruction in instructions)
+                for (var i = 1; i <= instruction.Cycle; i++)
+                    registers.Add(i == instruction.Cycle
+                        ? new State(instruction, registers.Last().To, registers.Last().To + instruction.Value)
+                        : new State(instruction, registers.Last().To, registers.Last().To));
+
+            return registers;
         }
 
         private static void PrettyPrint(char[] crtLine)
