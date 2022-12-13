50bfa73eec796066ac56276caedd1be2ddc908d6
Simon Alphonse <IAmSimon@outlook.in>
2022-12-13 10:44:27 +0530

    Start Day 13

--- 2022/12.HillClimbingAlgorithm/Program.cs
+++ 2022/12.HillClimbingAlgorithm/Program.cs
@@ -13,68 +13,72 @@ public abstract class Program
 
     record Path(List<Cell> Steps)
     {
-        public bool? IsSuccess { get; set; }
+        public bool IsSuccess { get; set; }
+        public List<Cell> Travelled { get; set; } = new();
     }
 
     public static void Main()
     {
-        var inputs = File.ReadAllLines("inputs-demo.txt")
-            .Select(s => s.Replace('S', 'z').ToCharArray()).ToArray();
+        var inputs = File.ReadAllLines("inputs-demo.txt").Select(s=> s.ToCharArray()).ToArray();
 
-        Cell start = new(Find(inputs, 'z'), 'z'),
-            destination = new(Find(inputs, 'E'), 'E');
+        Cell start = new(Find(inputs, 'S'), 'a'), 
+            destination = new(Find(inputs, 'E'), 'z');
+        
+        inputs[start.Point.Y][start.Point.X] = 'z';
+        inputs[destination.Point.Y][destination.Point.X] = 'z';
 
         var paths = new List<Path> { new(new List<Cell>() { start }) };
 
         GetClimbPaths(inputs, new(start, destination), ref paths);
 
-        Console.WriteLine($"Part One : {paths.Where(w => w.IsSuccess ?? false).Min(x => x.Steps.Count)}");
+        Console.WriteLine($"Part One : {paths.Where(w => w.IsSuccess).Min(x => x.Steps.Count)}");
     }
 
     private static void GetClimbPaths(char[][] inputs, Bookmark bookmark, ref List<Path> paths)
     {
-        var path = paths.First(f => f.IsSuccess == null);
+        var path = paths.First(f => !f.IsSuccess);
         var from = path.Steps.Last();
-        var directions = GetDirections(inputs, from.Point)
-            .Where(w => path.Steps.All(a => a.Point != w.Point)).ToArray();
+        var directions = GetDirections(inputs, from)
+            .Where(w => path.Steps.All(a => a.Point != w.Point) && 
+                        path.Travelled.All(a => a.Point != w.Point) &&
+                        from.Value + 1 >= w.Value).ToArray();
 
         if (directions.Any(cell => cell.Point == bookmark.Destination.Point))
         {
             path.Steps.Add(bookmark.Destination);
             path.IsSuccess = true;
-            return;
+            
+            paths.Add(new Path(new List<Cell>() { bookmark.Start }));
+            GetClimbPaths(inputs, bookmark, ref paths);
         }
 
-        if (directions.Any(x => from.Value + 1 >= x.Value))
+        if (directions.Any())
         {
             foreach (var direction in directions)
             {
-                if (!CheckIfPathIsTravelled(paths, path, direction))
-                {
-                    path.Steps.Add(direction);
-                    GetClimbPaths(inputs, bookmark, ref paths);
-                }
+                path.Steps.Add(direction);
+                GetClimbPaths(inputs, bookmark, ref paths);
             }
         }
-        
-        path.IsSuccess = false;
-        paths.Add(new Path(new List<Cell>() { bookmark.Start }));
+        else
+        {
+            path.Steps.Remove(from);
+            path.Travelled.Add(from);
+            GetClimbPaths(inputs, bookmark, ref paths);
+        }
     }
 
-    private static bool CheckIfPathIsTravelled(List<Path> paths, Path path, Cell cell)
-    {
-        return paths.Any(x => x.IsSuccess != null && string.Concat(x.Steps.Select(s => s.Point).Select(s => $"({s.X}{s.Y})"))
-            .StartsWith($"{string.Concat(path.Steps.Select(s => s.Point).Select(s => $"({s.X}{s.Y})"))}{cell.Point.X}{cell.Point.Y}"));
-    }
-
-    private static Cell[] GetDirections(char[][] inputs, Point point)
+    private static Cell[] GetDirections(char[][] inputs, Cell cell)
     {
         var cells = new List<Cell>(); // NEWS
-        if (point.Y > 0) cells.Add(new(point with { Y = point.Y - 1 }, inputs[point.Y - 1][point.X]));
-        if (point.X < inputs.Length - 1) cells.Add(new(point with { X = point.X + 1 }, inputs[point.Y][point.X + 1]));
-        if (point.X > 0) cells.Add(new(point with { X = point.X - 1 }, inputs[point.Y][point.X - 1]));
-        if (point.Y < inputs[0].Length - 1)
-            cells.Add(new(point with { Y = point.Y + 1 }, inputs[point.Y + 1][point.X]));
+        if (cell.Point.Y > 0) /* ^ */
+            cells.Add(new(cell.Point with { Y = cell.Point.Y - 1 }, inputs[cell.Point.Y - 1][cell.Point.X]));
+        if (cell.Point.X < inputs[0].Length - 1) /* > */
+            cells.Add(new(cell.Point with { X = cell.Point.X + 1 }, inputs[cell.Point.Y][cell.Point.X + 1]));
+        if (cell.Point.X > 0) /* < */
+            cells.Add(new(cell.Point with { X = cell.Point.X - 1 }, inputs[cell.Point.Y][cell.Point.X - 1]));
+        if (cell.Point.Y < inputs.Length - 1) /* V */
+            cells.Add(new(cell.Point with { Y = cell.Point.Y + 1 }, inputs[cell.Point.Y + 1][cell.Point.X]));
         return cells.ToArray();
     }
 
@@ -82,7 +86,8 @@ public abstract class Program
     {
         for (var y = 0; y < inputs.Length; y++)
             for (var x = 0; x < inputs[y].Length; x++)
-                if (inputs[y][x] == find) return new Point(x, y);
+                if (inputs[y][x] == find)
+                    return new(x, y);
 
         throw new KeyNotFoundException(find.ToString());
     }
---
+++ 2022/13.DistressSignal/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DistressSignal;
+
+abstract class Program
+{
+    record Signal(string Left, string Right);
+    public static void Main()
+    {
+        var inputs = File.ReadAllText("inputs-demo.txt")
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(s => new Signal(
+                s.Split(Environment.NewLine).First(), 
+                s.Split(Environment.NewLine).Last())
+            ).ToArray().ToArray();
+
+        
+        
+        
+        Console.Read();
+    }
+}
