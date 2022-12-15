17b4f20bd6fc897d43dbe84347e8f40ecb299eae
Simon Alphonse <IAmSimon@outlook.in>
2022-12-15 11:49:13 +0530

    Day 15 Start

---
+++ 2022/15.BeaconExclusionZone/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace BeaconExclusionZone;
+
+internal abstract class Program
+{
+    record Spot(Point From, Point To);
+
+    public static void Main()
+    {
+        var inputs = File.ReadAllLines($"inputs-demo.txt")
+            .Select(a => a.Split(' '))
+            .Select(b => new Spot(
+                new(int.Parse(b[2][2..^1]), int.Parse(b[3][2..^1])),
+                new(int.Parse(b[8][2..^1]), int.Parse(b[9][2..]))));
+
+        
+    }
+}
