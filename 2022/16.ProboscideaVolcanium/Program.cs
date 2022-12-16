79d71f73d1150f7aa9f308a86511de945155048d
Simon Alphonse <IAmSimon@outlook.in>
2022-12-16 14:13:49 +0530

    Start Day 16

---
+++ 2022/16.ProboscideaVolcanium/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ProboscideaVolcanium;
+
+internal abstract class Program
+{
+    record Valve(string Id, int FlowRate, string[] Valves);
+
+    public static void Main()
+    {
+        var inputs = File.ReadAllLines("inputs-demo.txt")
+            .Select(s => s.Replace(";", string.Empty).Replace(",", string.Empty)
+                .Split(' ')).Select(s => new Valve(s[1], int.Parse(s[4][5..]), s[9..])).ToArray();
+
+    }
+}
