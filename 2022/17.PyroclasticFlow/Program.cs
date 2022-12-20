d6485e3507b61430e10393c33b9536184b58e0ba
Simon Alphonse <IAmSimon@outlook.in>
2022-12-20 11:44:27 +0530

    Temporary commit

--- 2022/17.PyroclasticFlow/Program.cs
+++ 2022/17.PyroclasticFlow/Program.cs
@@ -5,9 +5,6 @@ namespace PyroclasticFlow;
 
 internal abstract class Program
 {
-    record Line(char[] Characters, Point Point);
-    record Dimension(int Height, int Width);
-
     public static void Main()
     {
         var wind = File.ReadAllText("inputs-demo.txt")
@@ -15,77 +12,48 @@ internal abstract class Program
 
         var shapes = new[]
         {
-            new[,]
-            {
-                { '#', '#', '#', '#' }
-            },
-            new[,]
-            {
-                { '.', '#', '.' },
-                { '#', '#', '#' },
-                { '.', '#', '.' },
-            },
-            new[,]
-            {
-                { '.', '.', '#' },
-                { '.', '.', '#' },
-                { '#', '#', '#' },
-            },
-            new[,]
-            {
-                { '#' },
-                { '#' },
-                { '#' },
-                { '#' },
-            },
-            new[,]
-            {
-                { '#', '#' },
-                { '#', '#' },
-            },
+            new[,] { { '#', '#', '#', '#' } },
+            new[,] { { '.', '#', '.' }, { '#', '#', '#' }, { '.', '#', '.' }, },
+            new[,] { { '.', '.', '#' }, { '.', '.', '#' }, { '#', '#', '#' }, },
+            new[,] { { '#' }, { '#' }, { '#' }, { '#' }, },
+            new[,] { { '#', '#' }, { '#', '#' }, },
         };
 
-        int wave = 0, rocks = 2022;
-        int width = 7, height = shapes.Max(s => s.GetLength(0)) * rocks;
-        var space = CreateSpace(new(width, height), '.');
-
-        Point gap = new(3, 2), position = gap with { X = height - gap.X };
-
-        for (var rock = 0; rock < rocks;)
+        var points = shapes.Select(s => s.FindPoints('#')).ToArray();
+        int wave = 0, height = shapes.Max(s => s.GetLength(0)) * 2022;
+        Point gap = new(3, 2), start = gap with { X = height - gap.X }, position = start;
+        var space = '.'.CreateArray(7, height);
+        
+        for (var rock = 0; rock < 2022;)
         {
             position = ChangePositionByWind(position, space, wind, wave++);
 
-            var floor = GetFloorIndex(space,'#');
-            var shape = shapes[rock % shapes.Length];
+            var shapeNo = rock % shapes.Length;
+            var @base = GetBase(space, shapes[shapeNo], position);
+            var shapePoints = points[shapeNo];
+            var basePoints = @base.FindPoints('#');
 
-            var @base = Enumerable.Range(position.Y, shape.GetLength(1))
-                    .Select(x => space[position.X + shape.GetLength(0) + 1, x]).ToArray();
-            
-            //floor > height ? new[] { '.', '.', '.' }
-            
-            space.Export("PartOne.txt");
-
-            if (IsOverlap(new Line[] { }))
+            if (basePoints.Length > 0)
             {
-                space = DrawShape(space, shape, position, '#');
+                space = space.DrawShape(shapes[shapeNo], position, '#');
+                position = position with { X = position.X - gap.X, Y = 2 };
                 rock++;
             }
             else
             {
                 position = position with { X = position.X + 1 };
             }
+
+            space.Export("PartOne.txt");
         }
     }
 
-    private static bool IsOverlap(IEnumerable<Line> shapes)
+    private static char[,] GetBase(char[,] space, char[,] shape, Point position)
     {
-        
-        return default;
-    }
-
-    private static int GetFloorIndex(char[,] space, char @char)
-    {
-        return space.GetLength(0) - (space.Find(@char, out var found) ? found.X : -1);
+        var @base = new char[1, shape.GetLength(1)]; // todo only consider # on bottom of the row of shape
+        for (var column = 0; column < @base.GetLongLength(1); column++)
+            @base[0, column] = space[position.X + shape.GetLength(0), column];
+        return @base;
     }
 
     private static Point ChangePositionByWind(Point position, char[,] space, int[] wind, int index)
@@ -96,27 +64,6 @@ internal abstract class Program
     private static int GetWindForce(int[] wind, int index)
     {
         return Math.Sign(wind[index % wind.Length]);
-    }
-
-    private static char[,] DrawShape(char[,] space, char[,] shape, Point position, char @char)
-    {
-
-        return default;
-    }
-
-    private static char[,] CreateSpace(Dimension dimension, char @char)
-    {
-        var space = new char[dimension.Width, dimension.Height];
-
-        for (var x = 0; x < space.GetLength(0); x++)
-        {
-            for (var y = 0; y < space.GetLength(1); y++)
-            {
-                space[x, y] = @char;
-            }
-        }
-
-        return space;
     }
 }
 
@@ -144,7 +91,7 @@ public static class ArrayExtensions
         return !position.IsEmpty;
     }
     
-    public static bool FindAll<T>(this T[,] array, T value, out Point[] positions) where T : struct
+    public static Point[] FindPoints<T>(this T[,] array, T value) where T : struct
     {
         ConcurrentBag<Point> points = new();
 
@@ -159,16 +106,33 @@ public static class ArrayExtensions
             }
         });
 
-        positions = points.ToArray();
-
-        return !points.IsEmpty;
+        return points.ToArray();
     }
     
-    public static void Export<T>(this T[,] array, string fileName)
+    public static char[,] DrawShape(this char[,] space, char[,] shape, Point point, char @char)
     {
-        File.Delete(fileName);
+        
+        return default;
+    }
+    
+    public static char[,] CreateArray(this char @char, int rows, int columns)
+    {
+        var space = new char[columns, rows];
 
-        using StreamWriter file = new(fileName, append: true);
+        for (var x = 0; x < space.GetLength(0); x++)
+        {
+            for (var y = 0; y < space.GetLength(1); y++)
+            {
+                space[x, y] = @char;
+            }
+        }
+
+        return space;
+    }
+    
+    public static void Export<T>(this T[,] array, string fileName, bool append = false)
+    {
+        using StreamWriter file = new(fileName, append);
 
         for (var i = 0; i < array.GetLength(0); i++)
         {
