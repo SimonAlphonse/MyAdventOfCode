1383f8117579abd9d5e86fa330370817e4f581b6
Tamil Selvi Arunachalam <tarunachalam@ntst.com>
2023-12-05 20:39:42 +0530

    Part One BigInt Approach

--- 2023/C#/05 IfYouGiveASeedAFertilizer/Program.cs
+++ 2023/C#/05 IfYouGiveASeedAFertilizer/Program.cs
@@ -1,36 +1,11 @@
-﻿using System;
-using System.Linq;
+﻿using Extensions;
 using System.Numerics;
 
 var lines = File.ReadAllText("sample.txt")
     .Split($"{Environment.NewLine}{Environment.NewLine}");
 
-var seeds = lines[0][7..].Split(' ').Select(int.Parse);
-var maps = lines[1..].Select(s => s.Split(':').Last()).Select(s =>
-    s.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-        .Select(s => s.Split(' ').Select(int.Parse).ToArray()).ToArray());
-
-var lookup = maps.Select(s => s.SelectMany(s => Enumerable.Range(0, s[2])
-            .ToDictionary(i => s[1] + i, i => s[0] + i)).ToDictionary()).ToArray();
-
-List<int> locations = [];
-
-int cache = 0;
-
-foreach (var seed in seeds)
-{
-    cache = seed;
-
-    foreach (var map in lookup)
-    {
-        if (map.ContainsKey(cache))
-        {
-            cache = map[cache];
-        }
-    }
-
-    locations.Add(cache);
-}
+var lookup = GetLookup<BigInteger>(lines[1..]).ToArray();
+var locations = GetLocations(lines[0], lookup);
 
 Console.WriteLine($"Part One : {locations.Min()}");
 
@@ -38,13 +13,38 @@ Console.WriteLine($"Part One : {locations.Min()}");
 
 Console.Read();
 
+IEnumerable<T> GetLocations<T>(string seeds, Dictionary<T, T>[] lookup) where T : INumber<T>
+{
+    foreach (var seed in seeds[7..].Split(' ').Select(s => T.Parse(s, default)))
+    {
+        T cache = seed;
+
+        foreach (var map in lookup)
+            if (map.TryGetValue(cache, out T value))
+                cache = value;
+
+        yield return cache;
+    };
+}
+
+IEnumerable<Dictionary<T, T>> GetLookup<T>(IEnumerable<string> lines) where T:INumber<T>
+{
+    foreach (var line in lines.Select(s => s.Split(':').Last()))
+    {
+        yield return line.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s.Split(' ').Select(s => T.Parse(s, default)).ToArray())
+            .SelectMany(s => GenericExtensions.Range<T>(T.Zero, s[2])
+            .ToDictionary(i => s[1] + i, i => s[0] + i)).ToDictionary();
+    }
+}
+
 namespace Extensions
 {
-    public static class Enumerable
+    public static class GenericExtensions
     {
         public static IEnumerable<T> Range<T>(T start, T range) where T : INumber<T>
         {
-            for (var i = T.Zero; start < range; i++)
+            for (var i = T.Zero + start; i < start + range; i++)
                 yield return start + i;
         }
     }
