83b8a8fffe6f0d3aa9a08cb992296e00d487267d
Tamil Selvi Arunachalam <tarunachalam@ntst.com>
2023-12-06 13:18:37 +0530

    Part Two Done

--- 2023/C#/06 WaitForIt/Program.cs
+++ 2023/C#/06 WaitForIt/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Extensions;
+using System.Numerics;
 
 internal class Program
 {
@@ -7,32 +8,44 @@ internal class Program
         var inputs = File.ReadAllLines("sample.txt")
             .Select(s => s.Split(':').Last()
             .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            .Select(int.Parse).ToArray()).ToArray();
+            .Select(long.Parse).ToArray()).ToArray();
 
-        var records = Enumerable.Range(0, inputs.Length + 1)
-            .Select(i => (Time: inputs[0][i], Distance: inputs[1][i])).ToArray();
+        var records = GenericExtensions.Range<long>(0, inputs[0].Length)
+            .Select(i => new Race<long>(Time: inputs[0][i], Distance: inputs[1][i])).ToArray();
 
-        int[] newRecords = GetRecordCounts(records).ToArray();
+        Console.WriteLine($"Part One : {GetRecordCounts(records).Aggregate((a, b) => a * b)}");
 
-        Console.WriteLine($"Part One : {newRecords.Aggregate((a, b) => a * b)}");
+        var race = new Race<long>(long.Parse(string.Join(string.Empty, records.Select(s => s.Time))),
+            long.Parse(string.Join(string.Empty, records.Select(s => s.Distance))));
 
-        //Console.WriteLine($"Part Two : {}");
+        Console.WriteLine($"Part Two : {GetWinCount(race, 14)}");
 
         Console.Read();
     }
 
-    private static IEnumerable<int> GetRecordCounts((int Time, int Distance)[] records)
+    private static IEnumerable<int> GetRecordCounts<T>(Race<T>[] races) where T:INumber<T>
     {
-        foreach (var record in records)
-        {
-            yield return Enumerable.Range(0, record.Time)
-                .Select(hold => hold * (record.Time - hold))
-                .Where(w => w > record.Distance).Count();
-        }
+        foreach (var race in races)
+            yield return GetWinCount(race, T.Zero);
+    }
+
+    private static int GetWinCount<T>(Race<T> race, T start) where T : INumber<T>
+    {
+        return GenericExtensions.Range(start, race.Time + T.One).AsParallel()
+            .Select(hold => hold * (race.Time - hold)).Count<T>(w => w > race.Distance);
     }
 }
 
+internal record struct Race<T>(T Time, T Distance) where T : INumber<T>;
+
 namespace Extensions
 {
-   
+    public static class GenericExtensions
+    {
+        public static IEnumerable<T> Range<T>(T start, T range) where T : INumber<T>
+        {
+            for (var i = T.Zero; i < range; i++)
+                yield return start + i;
+        }
+    }
 }
